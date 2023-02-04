ee25c68a6c7c2167ae07f2d3a79a813fe826ec69
Conorsmine <79007923+Conorsmine@users.noreply.github.com>
2023-02-04 14:38:36 +0100

    Init Intellij project

---
+++ src/main/java/com/conorsmine/net/banbt/BaNBT.java
@@ -0,0 +1,17 @@
+package com.conorsmine.net.banbt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BaNBT extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
