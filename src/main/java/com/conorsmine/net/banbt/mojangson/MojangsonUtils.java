eadead72b0cbbdd5ee03ebd4c8ebc81a25ca22db
Conorsmine <79007923+Conorsmine@users.noreply.github.com>
2023-02-07 17:02:10 +0100

    Update MojangsonUtils.java
    Added/Fixed support for coloring specific paths.

--- src/main/java/com/conorsmine/net/banbt/mojangson/MojangsonUtils.java
+++ src/main/java/com/conorsmine/net/banbt/mojangson/MojangsonUtils.java
@@ -35,7 +35,7 @@ public class MojangsonUtils {
         return prettyString.create();
     }
 
-    private ComponentBuilder recursive(String path, NBTCompound compound, ComponentBuilder prettyString) {
+    private ComponentBuilder recursive(String path, final NBTCompound compound, ComponentBuilder prettyString) {
         final Iterator<String> compoundIterator = compound.getKeys().iterator();
         while (compoundIterator.hasNext()) {
             final String key = compoundIterator.next();
@@ -44,22 +44,23 @@ public class MojangsonUtils {
             boolean shouldColor = isColoring(newPath);
 
             if (type == NBTType.NBTTagCompound)
-                evaluateCompoundTag(compound, key, path, prettyString, compoundIterator.hasNext());
+                evaluateCompoundTag(compound, key, path, prettyString, !compoundIterator.hasNext(), shouldColor);
 
             else if (type == NBTType.NBTTagList)
-                evaluateCompoundList(compound, key, newPath, prettyString, compoundIterator.hasNext(), shouldColor);
+                evaluateCompoundList(compound, key, newPath, prettyString, !compoundIterator.hasNext(), shouldColor);
 
             else
-                evaluateSimpleCompound(compound, key, newPath, prettyString, compoundIterator.hasNext(), shouldColor);
+                evaluateSimpleCompound(compound, key, newPath, prettyString, !compoundIterator.hasNext(), shouldColor);
         }
 
         return prettyString;
     }
 
     private void evaluateSimpleCompound(final NBTCompound compound, String key, String path, ComponentBuilder prettyString, boolean lastCompound, boolean shouldColor) {
-        String evaluatedString = String.format(tagCol + "%s: " + valCol + "%s§f", key, evaluateSimpleCompoundToString(compound, key));
-        if (shouldColor) evaluatedString = String.format(specCol + "%s: " + valCol + "%s§f", key, evaluateSimpleCompoundToString(compound, key));
-        if (lastCompound) evaluatedString += ", ";
+        String evaluatedString;
+        if (shouldColor) evaluatedString = String.format(specCol + "%s: %s§f", key, evaluateSimpleCompoundToString(compound, key));
+        else evaluatedString = String.format(tagCol + "%s: " + valCol + "%s§f", key, evaluateSimpleCompoundToString(compound, key));
+        if (!lastCompound) evaluatedString += ", ";
 
         final TextComponent pathDisplay = new TextComponent(evaluatedString);
         if (hoverable) pathDisplay.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(path).create()));
@@ -68,54 +69,68 @@ public class MojangsonUtils {
     }
 
     private void evaluateCompoundList(final NBTCompound compound, String key, String path, ComponentBuilder prettyString, boolean lastCompound, boolean shouldColor) {
-        final TextComponent pathDisplay = new TextComponent(String.format(((shouldColor) ? specCol : tagCol) + "%s:" + arrCol + "[§f", key));
-        if (hoverable) pathDisplay.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(path + "[..]").create()));
-        if (clickable) pathDisplay.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format(cmdFormat + "[..]", path)));
+        String evaluatedString;
+        if (shouldColor) evaluatedString = String.format("%s%s[§f", specCol, key);
+        else evaluatedString = String.format("%s%s:%s[§f", tagCol, key, arrCol);
+
+        final TextComponent pathDisplay = new TextComponent(evaluatedString);
+        if (hoverable) pathDisplay.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(path).create()));
+        if (clickable) pathDisplay.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format(cmdFormat, path)));
         prettyString.append(pathDisplay);
 
         final Iterator<ReadWriteNBT> compoundListIterator = compound.getCompoundList(key).iterator();
-        int i = -1;
+        int i = 0;
         while (compoundListIterator.hasNext()) {
             final ReadWriteNBT readWriteNBT = compoundListIterator.next();
+            String newArrPath = String.format("%s[%d]", path, i);
+            boolean colorArrObj = isColoring(newArrPath);
+
+            if (colorArrObj) prettyString.append(String.format("%s{§f", specCol));
+            else prettyString.append(String.format("%s{§f", objCol));
+            recursive(newArrPath, (NBTCompound) readWriteNBT, prettyString);
+
+            if (colorArrObj) prettyString.append(String.format("%s}§f", specCol));
+            else prettyString.append(String.format("%s}§f", objCol));
+            if (compoundListIterator.hasNext()) prettyString.append(", §f");
 
             i++;
-
-            prettyString.append(objCol + "{§f");
-            recursive(String.format("%s[%d]", path, i), (NBTCompound) readWriteNBT, prettyString);
-
-            if (compoundListIterator.hasNext()) prettyString.append(objCol + "}§f, ");
-            else prettyString.append(objCol + "}§f");
         }
 
 
-        if (lastCompound) prettyString.append(arrCol + "]§f, ");
-        else prettyString.append(arrCol + "]§f");
+        if (shouldColor) prettyString.append(String.format("%s]§f", specCol));
+        else prettyString.append(String.format("%s]§f", arrCol));
+        if (!lastCompound) prettyString.append(", §f");
     }
 
-    private void evaluateCompoundTag(final NBTCompound compound, String key, String path, ComponentBuilder prettyString, boolean lastCompound) {
-        final TextComponent pathDisplay = new TextComponent(String.format(tagCol + "%s:" + objCol + "{", key));
+    private void evaluateCompoundTag(final NBTCompound compound, String key, String path, ComponentBuilder prettyString, boolean lastCompound, boolean shouldColor) {
+        String evaluatedString;
+        if (shouldColor) evaluatedString = String.format("%s%s:%s{", specCol, key, specCol);
+        else evaluatedString = String.format("%s%s:%s{", tagCol, key, objCol);
+
+        final TextComponent pathDisplay = new TextComponent(evaluatedString);
         String newPath = (StringUtils.isBlank(path)) ? key : String.format("%s.%s", path, key);
 
         if (hoverable) pathDisplay.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(newPath).create()));
         if (clickable) pathDisplay.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format(cmdFormat, newPath)));
         prettyString.append(pathDisplay);
 
-        recursive(String.format("%s.%s", path, key), compound.getCompound(key), prettyString);
+        recursive(newPath, compound.getCompound(key), prettyString);
 
-        if (lastCompound) prettyString.append(objCol + "}§f, ");
+        if (shouldColor) prettyString.append(specCol + "}§f");
         else prettyString.append(objCol + "}§f");
+        if (!lastCompound) prettyString.append(", ");
     }
 
     private String evaluateSimpleCompoundToString(final NBTCompound compound, String key) {
         final NBTType type = compound.getType(key);
 
         if (type == NBTType.NBTTagInt) return String.valueOf(compound.getInteger(key));
-        else if (type == NBTType.NBTTagLong) return compound.getLong(key) + valTypeCol.toString() + "l§f";
-        else if (type == NBTType.NBTTagByte) return compound.getByte(key) + valTypeCol.toString() + "b§f";
-        else if (type == NBTType.NBTTagFloat) return compound.getFloat(key) + valTypeCol.toString() + "f§f";
-        else if (type == NBTType.NBTTagShort) return compound.getShort(key) + valTypeCol.toString() + "s§f";
-        else if (type == NBTType.NBTTagDouble) return compound.getDouble(key) + valTypeCol.toString() + "d§f";
-        else if (type == NBTType.NBTTagString) return  "\"" + compound.getString(key) + valTypeCol.toString() + "\"§f";
+        else if (type == NBTType.NBTTagLong) return compound.getLong(key) + "l§f";
+        else if (type == NBTType.NBTTagByte) return compound.getByte(key) + "b§f";
+        else if (type == NBTType.NBTTagFloat) return compound.getFloat(key) + "f§f";
+        else if (type == NBTType.NBTTagShort) return compound.getShort(key) + "s§f";
+        else if (type == NBTType.NBTTagDouble) return compound.getDouble(key) + "d§f";
+        else if (type == NBTType.NBTTagString) return  "\"" + compound.getString(key) + "\"§f";
         else return "§cSOMETHING WENT WRONG§f";
     }
 
@@ -126,62 +141,31 @@ public class MojangsonUtils {
     ///////////////////////////////////////////////////////////////
 
     private boolean isColoring(String newPath) {
-        final String colorPath = getSimilarAny(newPath, specialColorPaths);
-        boolean isColorPath = !StringUtils.isBlank(colorPath);
-        final String arrPath = isAnyPathArr(specialColorPaths);
+        System.out.println(newPath);
+        for (String colPath : specialColorPaths) {
+            if (!removeArrIndexes(newPath).startsWith(removeArrIndexes(colPath))) continue;
+            if (!arrIndexMatch(newPath, colPath)) continue;
 
-        if (!StringUtils.isBlank(arrPath)) isColorPath = (newPath.matches(String.format("%s.+", pathToNonRegex(arrPath))));
-            return isColorPath;
-    }
-
-    private static String isAnyPathArr(final String[] paths) {
-        for (String path : paths) {
-            if (isArr(getLastKey(path))) return path;
-        }
-        return "";
-    }
-
-    private static String getSimilarAny(final String path, final String[] compareTo) {
-        for (String s : compareTo) {
-            if (isSimilar(path, s)) return path;
-        }
-        return "";
-    }
-
-    // Items[..].id  Items[0].id -> true     Items[0].id  Items[1].damage -> false
-    private static boolean isSimilar(final String path, final String compareTo) {
-        final String[] compareKeys = pathToKeys(compareTo);
-        final String[] pathKeys = pathToKeys(path);
-
-        if (path.equals(compareTo)) return true;
-        if (compareKeys.length != pathKeys.length) return false;
-
-        // Todo:
-        //  If compareTo is not an Array, but path is => Return false
-        //  If both are not an Array, but the tags aren't the same => Return false
-        //  If both are an Array, but pathKey is a "full Array" => Continue
-        //  If both are an Array, but don't have the same index => Return false
-
-        for (int i = 0; i < compareKeys.length; i++) {
-            final String compareKey = compareKeys[i];
-            final String pathKey = pathKeys[i];
-            boolean compareArr = isArr(compareKey);
-            boolean pathArr = isArr(pathKey);
-
-            if (compareArr != pathArr) return false;
-            if (!getArrayKeyValue(compareKey).equals(getArrayKeyValue(pathKey))) return false;
-            if (compareArr && isFullArray(compareKey)) continue;
-            if (compareArr) {
-                if (getIndexOfArrayKey(compareKey) != getIndexOfArrayKey(pathKey)) return false;
-            }
-
+            return true;
         }
 
+        return false;
+    }
+
+    private boolean arrIndexMatch(String newPath, String colPath) {
+        String[] pathKeyArr = pathToKeys(newPath);
+        String[] colKeyArr = pathToKeys(colPath);
+
+        for (int i = 0; i < colKeyArr.length; i++) {
+            String pathKey = pathKeyArr[i];
+            String colKey = colKeyArr[i];
+
+            if (!isArr(pathKey)) continue;
+            if (!isArr(colKey)) return false;
+            if (isFullArray(colKey)) continue;
+            if (getIndexOfArrayKey(pathKey) != getIndexOfArrayKey(colKey)) return false;
+        }
         return true;
-    }
-
-    public static String pathToNonRegex(final String path) {
-        return path.replaceAll("\\.", "\\\\.").replaceAll("\\[", "\\\\[");
     }
 
     // Items[0].tag.Items[..] -> {Items[0], tag, Items[..]}
@@ -205,13 +189,14 @@ public class MojangsonUtils {
         return key.matches(".+\\.]$");
     }
 
-    public static String getArrayKeyValue(final String key) {
-        return key.replaceAll("\\[\\d]$", "").replaceAll("\\[\\.{2}]$", "");
-    }
-
     // Items[0] -> 0
     public static int getIndexOfArrayKey(final String key) {
         return Integer.parseInt(key.replaceAll(".+\\[|]$", ""));
+    }
+
+    // Items[..] -> Items   tag.ench[0].custom[..] -> tag.ench[].custom[]
+    public static String removeArrIndexes(final String key) {
+        return key.replaceAll("\\[((\\d+)|(\\.\\.))]", "[]");
     }
 
 
@@ -309,7 +294,7 @@ public class MojangsonUtils {
         return this;
     }
 
-    public MojangsonUtils setSpecialColorPaths(String[] specialColorPaths) {
+    public MojangsonUtils setSpecialColorPaths(String... specialColorPaths) {
         this.specialColorPaths = specialColorPaths;
         return this;
     }
