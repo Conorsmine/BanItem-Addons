9a8b088c4af53a51155396dac7b733aa9b25cfa5
Conorsmine <79007923+Conorsmine@users.noreply.github.com>
2023-02-07 23:20:23 +0100

    EOS
    Added most of the add command.
    Now I gotta figure out how the item/data is stored in the "customitemsconfig.yml" cause it seems that it is not automatically added to it.
    In addition the adding needs some polishing, surely some bug fixes and generally needs to be more responsive. Adding some sounds/simple info should help alot.

--- src/main/java/com/conorsmine/net/banbt/AddAction.java
+++ src/main/java/com/conorsmine/net/banbt/AddAction.java
@@ -1,45 +1,204 @@
 package com.conorsmine.net.banbt;
 
+import de.tr7zw.changeme.nbtapi.NBTContainer;
 import de.tr7zw.changeme.nbtapi.NBTItem;
+import de.tr7zw.changeme.nbtapi.NBTType;
+import fr.andross.banitem.BanConfig;
+import fr.andross.banitem.BanItem;
+import fr.andross.banitem.actions.BanAction;
+import fr.andross.banitem.actions.BanActionData;
+import fr.andross.banitem.actions.BanDataType;
+import fr.andross.banitem.database.items.CustomItems;
+import fr.andross.banitem.items.CustomBannedItem;
+import fr.andross.banitem.utils.debug.Debug;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.World;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AddAction {
 
     private static final Map<UUID, AddAction> actionMap = new HashMap<>();
+    private static final String finishActionCmdFormat = "/bn hidden_cmd %s FINISH";
 
     private final BaNBT pl;
     private final Player p;
-    private final UUID actionID;
-    private Set<String> compareStrings = new HashSet<>();
+    private final UUID actionID = UUID.randomUUID();
+    private final String header;
 
-    public AddAction(BaNBT pl, Player p, String cmdHeader) {
+    private final boolean banCurrent;
+    private final Set<BanAction> banActions;
+    private final World[] banWorlds;
+    private final List<String> banMessages;
+    private final Set<String> nbtDataStrings = new HashSet<>();
+    private final NBTContainer itemNBT;
+
+    private final MojangsonUtils mojangson = new MojangsonUtils()
+            .setClickTypes(NBTType.NBTTagByte, NBTType.NBTTagDouble, NBTType.NBTTagFloat, NBTType.NBTTagInt, NBTType.NBTTagLong, NBTType.NBTTagShort, NBTType.NBTTagString)
+            .setCmdFormat("/bn hidden_cmd " + actionID + " %s")
+            .setInvalidClickTargetFormat("/bn hidden_cmd " + actionID + " INVALID %s ");
+
+    public AddAction(BaNBT pl, Player p, String cmdHeader, Set<BanAction> banActions, World[] banWorlds, List<String> banMessages, boolean banCurrent) {
         this.pl = pl;
         this.p = p;
-        this.actionID = UUID.randomUUID();
+        this.header = cmdHeader;
+        this.itemNBT = NBTItem.convertItemtoNBT(p.getInventory().getItemInMainHand());
+
+        this.banCurrent = banCurrent;
+        this.banActions = banActions;
+        this.banWorlds = banWorlds;
+        this.banMessages = banMessages;
 
         actionMap.put(actionID, this);
-        initAction(cmdHeader);
+        initAction();
     }
 
-    private void initAction(String header) {
+    private void initAction() {
+        sendUsageMsg();
+        p.spigot().sendMessage(mojangson.getInteractiveMojangson(itemNBT, ""));
+    }
+
+    private void deleteAction() {
+        actionMap.remove(actionID);
+    }
+
+    private void clearChat() {
+        p.sendMessage(new String[20]);
+    }
+
+    private void sendHeader() {
         p.sendMessage(pl.getCfgFile().getPrefix() + header);
-        p.spigot().sendMessage(
-                new MojangsonUtils()
-                        .setCmdFormat("/bn hidden_cmd %s " + actionID.toString())
-                        .getInteractiveMojangson(NBTItem.convertItemtoNBT(p.getInventory().getItemInMainHand()), "")
+    }
+
+    private void sendUsageMsg() {
+        final String PREFIX = pl.getCfgFile().getPrefix();
+        sendHeader();
+        p.sendMessage(String.format("%s§7Usage:", PREFIX));
+        p.sendMessage(String.format("%s§7 >> Select in the following NBT", PREFIX));
+        p.sendMessage(String.format("%s§7 >> all data that should be considered.", PREFIX));
+        p.sendMessage(String.format("%s§7 >> After doing so specify what the value", PREFIX));
+        p.sendMessage(String.format("%s§7 >> it should be.", PREFIX));
+        p.sendMessage(String.format("%s§7 >> When all the data has been selected", PREFIX));
+        p.sendMessage(String.format("%s§7 >> and configured, press on the §a\"FINISH\"§7 button.", PREFIX));
+        p.sendMessage(String.format("%s§7 >> Note: You can undo a selection by clicking on the path again.", PREFIX));
+        p.sendMessage(String.format("%s§7 >> Note: You can only select data which has §aGREEN§7 hovertext.", PREFIX));
+        p.sendMessage(String.format("%s§7 >> Note: This process will take up your chat.", PREFIX));
+        p.sendMessage(String.format("%s§7 >> Note: Use §b/bn info §7to get basic info.", PREFIX));
+    }
+
+    private void sendNewDataMsg() {
+        clearChat();
+        sendHeader();
+        p.spigot().sendMessage(mojangson
+                .setSpecialColorPaths(nbtDataStrings.toArray(new String[0]))
+                .getInteractiveMojangson(itemNBT, "")
         );
+
+        p.sendMessage("");
+        final TextComponent finishButton = new TextComponent("FINISH");
+        finishButton.setColor(ChatColor.GREEN);
+        finishButton.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format(finishActionCmdFormat, actionID)));
+        finishButton.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Finished configuring?").create()));
+        p.spigot().sendMessage(finishButton);
+    }
+
+    private void sendInvalidTargetErr(String[] args) {
+        p.sendMessage(String.format("%s §7\"§b%s§7\" is not a valid path!", pl.getCfgFile().getPrefix(), args[3]));
+        p.sendMessage(String.format("%s §7All valid data options have §aGREEN§7 hover text.", pl.getCfgFile().getPrefix()));
+    }
+
+    private void toggleDataString(String data) {
+        if (nbtDataStrings.contains(data))
+            nbtDataStrings.remove(data);
+        else
+            nbtDataStrings.add(data);
+    }
+
+    private void completeAction(CommandSender sender) {
+        pl.getBanItemAPI().getCustomItems()
+                .put("smth", createCustomBannedItem(sender));
+
+        pl.getBanItemAPI().addToBlacklist(
+                createCustomBannedItem(sender),
+                createBanActionData(),
+                banWorlds
+        );
+        BanItem.getInstance().getListener().load(sender);
+
+        p.sendMessage(String.format("%s %s", pl.getCfgFile().getPrefix(), header));
+        // todo:
+        p.sendMessage("Send some completion text here!");
+        deleteAction();
+    }
+
+    private YamlConfiguration createConfigurationSection() {
+        final YamlConfiguration nbtData = new YamlConfiguration();
+        for (String path : nbtDataStrings) {
+            // There is a weird quirk about the BanItem plugin that only "acknowledges" the "Damage"
+            // if it's in the main configuration section
+            if (path.equals("Damage")) continue;
+            MojangsonUtils.NBTResult compoundResult = MojangsonUtils.getCompoundFromPath(itemNBT, path);
+            Object dataFromNBT = MojangsonUtils.getSimpleDataFromCompound(compoundResult);
+
+            nbtData.set(
+                    path.replaceAll("\\.", "#"),
+                    dataFromNBT
+            );
+        }
+
+        final YamlConfiguration customItemConf = new YamlConfiguration();
+        if (nbtDataStrings.contains("Damage")) customItemConf.set("damage", nbtDataStrings.stream().filter(s -> s.equals("Damage")).findFirst().get());
+        customItemConf.set("material", NBTItem.convertNBTtoItem(itemNBT).getType().name().toLowerCase(Locale.ROOT));
+        customItemConf.set("nbtapi", nbtData);
+        return customItemConf;
+    }
+
+    private CustomBannedItem createCustomBannedItem(CommandSender sender) {
+        BanItem banItemPlugin = BanItem.getInstance();
+        Debug itemDebug = new Debug(banItemPlugin.getBanConfig(), sender);
+        return new CustomBannedItem("smth", createConfigurationSection(), itemDebug);
+    }
+
+    private Map<BanAction, BanActionData> createBanActionData() {
+        final Map<BanAction, BanActionData> actionData = new HashMap<>();
+        final BanActionData banActionData = new BanActionData();
+        banActionData.getMap().put(BanDataType.MESSAGE, banMessages);
+
+        banActions.forEach(banAction -> actionData.put(banAction, banActionData));
+        return actionData;
     }
 
 
 
     public static void processActionCmd(CommandSender sender, String[] args) {
-        System.out.println("HERE");
+        final AddAction action = getActionMap().getOrDefault(UUID.fromString(args[1]), null);
+        if (action == null) return;
+        if (args[2].equals("INVALID")) { action.sendInvalidTargetErr(args); return; }
+        if (args[2].equals("FINISH")) { action.completeAction(sender); return; }
+
+        action.toggleDataString(args[2]);
+        action.sendNewDataMsg();
     }
 
     public static Map<UUID, AddAction> getActionMap() {
         return actionMap;
+    }
+
+    public Player getP() {
+        return p;
+    }
+
+    public Set<String> getNbtDataStrings() {
+        return nbtDataStrings;
     }
 }
--- src/main/java/com/conorsmine/net/banbt/cmds/AddCmd.java
+++ src/main/java/com/conorsmine/net/banbt/cmds/AddCmd.java
@@ -3,14 +3,14 @@ package com.conorsmine.net.banbt.cmds;
 import com.conorsmine.net.banbt.BaNBT;
 import com.conorsmine.net.banbt.AddAction;
 import fr.andross.banitem.actions.BanAction;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class AddCmd extends AbstractBanNBTCmd{
@@ -28,7 +28,23 @@ public class AddCmd extends AbstractBanNBTCmd{
 
         if (AddAction.getActionMap().containsKey(p.getUniqueId())) sendLastActionCanceledMsg(sender);
         sender.sendMessage(String.format("%s§aStarted new action.", getPlugin().getCfgFile().getPrefix()));
-        new AddAction(getPlugin(), p, getHeader());
+
+        Set<BanAction> banActions = parseBanActions(args, sender);
+        World[] banWorlds = parseBanWorlds(args, sender);
+        List<String> banMessages = parseBanMessages(args, sender);
+        boolean useCurrent = parseBanItemCurrently(args, sender);
+
+        if (banActions.isEmpty()) { sendNonActionErr(sender); return; }
+        if (banWorlds.length == 0) banWorlds = new World[] { p.getWorld() };
+        new AddAction(
+                getPlugin(),
+                p,
+                getHeader(),
+                banActions,
+                banWorlds,
+                banMessages,
+                useCurrent
+        );
     }
 
     @Override
@@ -38,19 +54,90 @@ public class AddCmd extends AbstractBanNBTCmd{
         return null;
     }
 
+    private Set<BanAction> parseBanActions(String[] args, CommandSender sender) {
+        Set<String> allActions = Arrays.stream(BanAction.values()).map(BanAction::getName).collect(Collectors.toSet());
+        Set<BanAction> banActions = new HashSet<>();
+        for (int i = 1; i < args.length; i++) {
+            String s = args[i].replace(",", "");
+            if (s.equals("-w") || s.equals("-m") || s.equals("-b")) break;
+            if (allActions.contains(s))
+                banActions.add(BanAction.valueOf(s.toUpperCase(Locale.ROOT)));
+            else
+                sender.sendMessage(String.format("%s §7\"§3%s§7\" is not a valid action.", getPlugin().getCfgFile().getPrefix(), s));
+        }
+
+        return banActions;
+    }
+
+    private World[] parseBanWorlds(String[] args, CommandSender sender) {
+        boolean found = false;
+        Set<String> worldNames = getPlugin().getServer().getWorlds().stream().map(World::getName).collect(Collectors.toSet());
+        Set<World> banWorlds = new HashSet<>();
+        for (String s : args) {
+            if (s.equals("-w")) found = true;
+            if (s.equals("-m") || s.equals("-b")) break;
+            if (!found) continue;
+            if (worldNames.contains(s))
+                banWorlds.add(getPlugin().getServer().getWorld(s));
+            else
+                sender.sendMessage(String.format("%s §7\"§3%s§7\" is not a valid world.", getPlugin().getCfgFile().getPrefix(), s));
+        }
+
+        return banWorlds.toArray(new World[0]);
+    }
+
+    private List<String> parseBanMessages(String[] args, CommandSender sender) {
+        boolean found = false;
+        List<String> banMessages = new LinkedList<>();
+        for (String s : args) {
+            if (s.equals("-m")) found = true;
+            if (s.equals("-w") || s.equals("-b")) break;
+            if (!found) continue;
+            banMessages.add(s);
+        }
+
+        return banMessages;
+    }
+
+    private boolean parseBanItemCurrently(String[] args, CommandSender sender) {
+        for (int i = 1; i < args.length - 2; i++) {
+            if (args[i].equals("-b")) continue;
+            if (args[i + 1].toLowerCase(Locale.ROOT).equals("true"))
+                return true;
+            else {
+                sender.sendMessage(String.format("%s §7\"§3%s§7\" is not a valid boolean value.", getPlugin().getCfgFile().getPrefix(), args[i] + 1));
+                sender.sendMessage(String.format("%s §7Defaulted to §3false§7.", getPlugin().getCfgFile().getPrefix()));
+            }
+
+        }
+
+        return false;
+    }
+
+
+
+
+
+
     private void sendUsageMsg(CommandSender sender) {
         final String PREFIX = getPlugin().getCfgFile().getPrefix();
         sender.sendMessage(PREFIX + getHeader());
         sender.sendMessage(String.format("%s§7Usage:", PREFIX));
-        sender.sendMessage(String.format("%s§b/bn add §3<actions> [-w worlds]", PREFIX));
+        sender.sendMessage(String.format("%s§b/bn add §3<actions> [-w worlds] [-m message] [-b asIs]", PREFIX));
         sender.sendMessage(String.format("%s§7 >> Will ban the currently held item.", PREFIX));
         sender.sendMessage(String.format("%s§7 >> If no worlds were entered,", PREFIX));
         sender.sendMessage(String.format("%s§7 >> it will default to the current.", PREFIX));
-        sender.sendMessage(String.format("%s§2 Example: /bn add hold,use -w world", PREFIX));
+        sender.sendMessage(String.format("%s§7 >> Set the §b-b§7 flag to §3true§7 to ban.", PREFIX));
+        sender.sendMessage(String.format("%s§7 >> the item as is. (A shortcut per say)", PREFIX));
+        sender.sendMessage(String.format("%s§2 Example: §b/bn add §3hold,use -w world§f", PREFIX));
     }
 
     private void sendNonItemErr(CommandSender sender) {
         sender.sendMessage(String.format("%s§cYou need to hold an item for this command!", getPlugin().getCfgFile().getPrefix()));
+    }
+
+    private void sendNonActionErr(CommandSender sender) {
+        sender.sendMessage(String.format("%s §cAt least one action to ban needs to be specified!", getPlugin().getCfgFile().getPrefix()));
     }
 
     private void sendLastActionCanceledMsg(CommandSender sender) {
